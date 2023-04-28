c760271ae586569a66f97ece9836f507ac86e4be
Federico <federuedaleal@hotmail.com>
2023-04-28 19:29:55 -0300

    Agregue la función de que cambie el icono dark/light

--- js/app.js
+++ js/app.js
@@ -8,4 +8,12 @@ btnTemaClaro.addEventListener('click', ()=> cambiarTema('light'))
 function cambiarTema(color){
     document.querySelector('html').setAttribute('data-bs-theme', color);
     localStorage.setItem('tema', JSON.stringify(color))
+    const icono = document.querySelector('.bi-moon-stars-fill')
+    if (color === 'dark') {
+        icono.classList.remove('bi-sun-fill')
+        icono.classList.add('bi-moon-stars-fill')
+    } else {
+        icono.classList.remove('bi-moon-stars-fill')
+        icono.classList.add('bi-sun-fill')
+    } 
 }
